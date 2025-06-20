49a7ff21b922edd8999bfd1c1bbb08e40d2d34d5
Manthan29-code <manthanmovaliya9@gmail.com>
2025-06-20 14:14:57 +0530

    create board design

---
+++ script.js
@@ -0,0 +1,83 @@
+const board = document.querySelector('.board');
+const button1 = document.getElementsByClassName('btn-1')[0];
+const button2 = document.getElementsByClassName('btn-2')[0];
+let currentPlayer;
+let currentMode;
+let isGameStarted=false;
+let gameBoard = [[],[],[]];
+
+button1.addEventListener('click', () => {
+    if(button1.innerText == "AI"){
+        currentMode="AI";
+        button1.style.display = "none";
+        button1.innerText = "Start";
+        button2.style.display = "block";
+        button2.innerText = "Reset";
+        currentPlayer="X";
+        alert("Player X Turn");
+    }else{
+        isGameStarted=true;
+        button1.innerText = "AI";
+        button2.style.display = "block";
+        button2.innerText = "Manual";
+    }   
+});
+
+function resetGame(){
+    currentPlayer=undefined;
+    currentMode=undefined;
+    gameBoard = [[],[],[]];
+    // let cells = document.querySelectorAll('.cell');
+    // cells.forEach(cell => {
+    //     cell.textContent = '';
+    // });
+    let cells = board.children;
+    for(let i=0;i<cells.length;i++){
+        cells[i].textContent = '';
+    }
+    
+}
+
+button2.addEventListener('click', () => {
+    if(button2.innerText == "Manual"){
+        currentMode="Manual";
+        button1.style.display = "none";
+        button1.innerText = "Start";
+        button2.style.display = "block";
+        button2.innerText = "Reset";
+        currentPlayer="X";
+        alert("Player X Turn");
+    }else{
+        button1.style.display = "block";
+        button1.innerText = "AI";
+        button2.innerText = "Manual";
+        resetGame();
+    }
+});
+
+board.addEventListener('click', (e) => {  
+    if(!isGameStarted){
+        alert("Please Start the Game");
+        return;
+    }
+    if(currentMode== undefined){
+        alert("Please Select the Mode");
+        return;
+    }  
+    console.log(e.target);
+    const cellelement = e.target;
+    let cell = e.target.id;
+    console.log(cell); 
+    cell=cell.split("_");
+    const row = parseInt(cell[0]);
+    const col = parseInt(cell[1]);
+    if(gameBoard[row][col] == undefined ){
+     gameBoard[row][col] = currentPlayer;
+     cellelement.innerText = currentPlayer;
+     currentPlayer = currentPlayer == "X" ? "O" : "X";
+    }
+    else{
+        alert("Already Filled");
+    }
+    console.log(row, col);   
+})
